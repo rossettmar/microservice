b18e66b83d60633ed9916077ec9992f53e471b24
marco <rossettmar@gmail.com>
2024-11-18 08:10:59 +0100

    First commit

---
+++ src/main/java/com/example/microservice/config/RabbitMQConfig.java
@@ -0,0 +1,49 @@
+package com.example.microservice.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.topic.exchange}")
+    private String topic;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routing_key;
+
+    // beanan per RabbitMQ queue
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+
+    // bean con definizione del topic
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(topic);
+    }
+
+    // binding coda -> exchange
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routing_key);
+    }
+
+    //Con springboot non c'è bisogno di configurare questi tre beans
+    // - ConnectionFactory
+    // - RabbitTemplate
+    // - RabbitAdmin
+
+}
---
+++ src/main/java/com/example/microservice/publisher/RabbitMQPRoducer.java
@@ -0,0 +1,31 @@
+package com.example.microservice.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQPRoducer {
+
+    @Value("${rabbitmq.topic.exchange}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routing_key;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQPRoducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    //constructor dependency injection
+    public RabbitMQPRoducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent -> %s", message ));
+        rabbitTemplate.convertAndSend(exchange, routing_key, message);
+    }
+
+}
